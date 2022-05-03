a620f40a5deb74f7c07716c3399bf232c140b52a
borgmon <borgmon7@gmail.com>
2022-05-03 21:32:04 +0000

    Updates

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 window.addEventListener('load', () => {
     var videoPlayer = new AVideoPlayer();
     var videoEl = document.getElementById("video-container")
+    var camera = document.querySelector("a-camera")
 
 
     function getLocation() {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition, error, { enableHighAccuracy: true });
         function showPosition(position) {
             console.log(position)
             videoEl.setAttribute("gps-entity-place", buildGPSEntity(position.coords.latitude, position.coords.longitude))
         }
+        function error(err) {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
     }
-    getLocation()
+    camera.addEventListener("gps-camera-update-positon", () => getLocation())
 })
 
 function buildGPSEntity(lat, long) {
