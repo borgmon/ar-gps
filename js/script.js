79ef007bbf4533b2764fb870b8e87dec8d843315
borgmon <borgmon7@gmail.com>
2022-05-25 05:07:00 +0000

    use marker

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 window.addEventListener('load', () => {
+    // var videoEl = document.getElementById("video-container")
+    var markerEl = document.querySelector("a-marker")
     var videoPlayer = new AVideoPlayer();
-    var videoEl = document.getElementById("video-container")
 
+    markerEl.addEventListener('markerFound', () => {
+        videoPlayer.elVideo.play()
+    });
 
-    function getLocation() {
-        navigator.geolocation.getCurrentPosition(showPosition);
-        function showPosition(position) {
-            console.log(position)
-            videoEl.setAttribute("gps-entity-place", buildGPSEntity(position.coords.latitude, position.coords.longitude))
-        }
-    }
+    // setTimeout(() => {
+    //     console.log('b')
+    //     var videoPlayer = new AVideoPlayer();
+    //     videoPlayer.elVideo.play()
+
+    // }, 1000);
+
+    // function getLocation() {
+    //     navigator.geolocation.getCurrentPosition(showPosition);
+    //     function showPosition(position) {
+    //         console.log(position)
+    //         videoEl.setAttribute("gps-entity-place", buildGPSEntity(position.coords.latitude, position.coords.longitude))
+    //     }
+    // }
     // getLocation()
 })
 
-function buildGPSEntity(lat, long) {
-    return "latitude: " + lat + "; longitude: " + long
-}
+// function buildGPSEntity(lat, long) {
+//     return "latitude: " + lat + "; longitude: " + long
+// }
